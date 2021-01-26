50b4831188ad2cb2c3ce4590dfb4ae3382d5aa89
Kamil Neczaj <kneczaj@gmail.com>
2021-01-26 23:27:08 +0200

    [CreateAdvert] elevate state to the page

---
+++ test/utils.ts
@@ -0,0 +1,16 @@
+import { act, fireEvent, screen } from "@testing-library/react";
+
+export const getButton = (label: string) => screen.getByRole('button', {name: new RegExp(label, 'i')});
+
+export async function clickButton(label: string) {
+  return act(async () => {
+    await fireEvent.click(getButton(label));
+  });
+}
+
+export function fillInput(label: string, value: any) {
+  const field = screen.getByLabelText(new RegExp(`^${label}$`, 'i'));
+  act(() => {
+    fireEvent.change(field, {target: {value}})
+  });
+}
