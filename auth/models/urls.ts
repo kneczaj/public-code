e69f470b16eb6644ed9209feca43c93a8305d9f7
Kamil Neczaj <kneczaj@gmail.com>
2021-09-16 03:02:56 +0200

    [Gateway] fix page reload

--- auth/models/urls.ts
+++ auth/models/urls.ts
@@ -1,4 +1,4 @@
-export const AUTH = '/auth';
+export const AUTH = '/authentication';
 export const LOGIN = `${AUTH}/login/`;
 export const REGISTER = `${AUTH}/register/`;
 export const GOOGLE_CALLBACK = `${AUTH}/google/callback`;
