dcdb1949de2b8c7d21c5700772f77577b26f8a6c
Kamil Neczaj <kneczaj@gmail.com>
2021-09-24 11:24:28 +0200

    note

--- components/provider-group.tsx
+++ components/provider-group.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { ReactNode } from 'react';
 
-export interface ProviderComponentProps {
-  children: ReactNode;
+export interface ProviderComponentProps<TContextProps = unknown> {
+  children: ReactNode | ((context: TContextProps) => React.ReactNode);
 }
 
 /**
