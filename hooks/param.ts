7f19aee15509783c51a0d41543b0a43c616631f0
Kamil Neczaj <kneczaj@gmail.com>
2021-10-28 16:14:45 +0200

    useParam - improve typing

--- hooks/param.ts
+++ hooks/param.ts
@@ -2,11 +2,18 @@ import { useParams } from 'react-router-dom';
 import { isUndefined } from 'public/util';
 import { RouterError } from 'app/router-utils';
 
-export function useParam(name: string): string {
-  const params = useParams<{ [K in string]?: string }>();
+export function useParam<
+  RoutingParams extends { [K in string]?: string },
+  TOut = string
+>(
+  name: string,
+  transformer: TOut extends string ? undefined : (original: string) => TOut
+): TOut extends string ? string : TOut {
+  const params = useParams<RoutingParams>();
   const value = params[name];
   if (isUndefined(value)) {
     throw new RouterError(`Required URL param "${name} not found"`);
   }
-  return value;
+  // @ts-ignore
+  return isUndefined(transformer) ? value : transformer(value);
 }
