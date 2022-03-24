999734204220e9e0b176667fca90aebd09a1cb39
Kamil Neczaj <kneczaj@gmail.com>
2022-03-25 00:31:36 +0200

    Remove old graphql code

--- graphql/utils.spec.ts
+++ graphql/utils.spec.ts
@@ -1,40 +1,10 @@
 import { MOCKS } from '../test';
-import { convertGQLErrors2Form, makeMutationRequest } from './utils';
-import { MutationFunctionOptions } from '@apollo/client/react/types/types';
-import { FetchResult } from '@apollo/client/link/core';
+import { convertGQLErrors2Form } from './utils';
 
 describe('convertGQLErrors2Form', () => {
   it('converts unauthorized error', () => {
     expect(convertGQLErrors2Form([MOCKS.unathorizedError])).toEqual(
       MOCKS.unauthorizedFormError
     );
-  });
-});
-
-describe('makeMutationRequest', function () {
-  const mock = {
-    mutation: jest.fn<
-      Promise<FetchResult<any>>,
-      [MutationFunctionOptions<any, any>]
-    >(() => Promise.resolve(MOCKS.fetchResultAllFieldsSuccess)),
-    errorMutation: jest.fn<
-      Promise<FetchResult<any>>,
-      [MutationFunctionOptions<any, any>]
-    >(() => Promise.resolve(MOCKS.errorResponse1))
-  };
-
-  it('passes all args when no errors', async () => {
-    const mutation = jest.spyOn(mock, 'mutation');
-    const request = makeMutationRequest(mutation as any);
-    expect(await request({})).toEqual(MOCKS.fetchResultAllFieldsSuccess);
-  });
-
-  it('interprets error from mutation with unauthorized error response', async () => {
-    const mutation = jest.spyOn(mock, 'errorMutation');
-    const request = makeMutationRequest(mutation as any);
-    expect(await request({})).toEqual({
-      ...MOCKS.errorResponse1,
-      errors: MOCKS.unauthorizedFormError
-    });
   });
 });
--- graphql/utils.ts
+++ graphql/utils.ts
@@ -1,7 +1,6 @@
-import { isNull, isUndefined } from '../util';
+import { isUndefined } from '../util';
 import { Config, FORM_ERROR, SubmissionErrors } from 'final-form';
 import { FetchResult } from '@apollo/client/link/core';
-import { MutationFunctionOptions } from '@apollo/client/react/types/types';
 import { GraphQLError } from 'graphql/error/GraphQLError';
 import {
   AuthenticationError,
@@ -86,71 +85,4 @@ export type FinalFormSubmissionResult<
 export interface FinalFormSubmissionMutationResult<TData, FormValues>
   extends Omit<FetchResult<TData>, 'errors'> {
   errors?: FinalFormSubmissionResult<FormValues>;
-}
-
-/**
- * Make a request to GraphQL API, and interpret errors no matter if they are returned or thrown.
- * The return format is with standard GraphQL data and loading properties and modified errors.
- * @param mutation
- */
-export function makeMutationRequest<TData, FormValues>(
-  mutation: (
-    options?: MutationFunctionOptions<TData, FormValues>
-  ) => Promise<FetchResult<TData>>
-): (
-  variables: FormValues
-) => Promise<FinalFormSubmissionMutationResult<TData, FormValues>> {
-  return async (
-    variables: FormValues
-  ): Promise<FinalFormSubmissionMutationResult<TData, FormValues>> => {
-    try {
-      const response = await mutation({ variables });
-      const baseErrors = isUndefined(response.errors)
-        ? undefined
-        : convertGQLErrors2Form(response.errors);
-      const errors = isNull(response.data)
-        ? {
-            [FORM_ERROR]: `Empty response from server`,
-            ...baseErrors
-          }
-        : baseErrors;
-      return isUndefined(errors)
-        ? response
-        : {
-            ...response,
-            errors
-          };
-    } catch (e: any) {
-      if (hasGraphQLErrors(e)) {
-        console.error(e);
-        return {
-          data: null,
-          errors: convertGQLErrors2Form(e.graphQLErrors)
-        };
-      }
-      return {
-        data: null,
-        errors: {
-          [FORM_ERROR]: isUndefined(e.message) ? 'unknown error' : e.message
-        }
-      };
-    }
-  };
-}
-
-/**
- * Converts a function which triggers the mutation from useMutation hook to a onSubmit function for final-form.
- * Use this wrapped with useMemo.
- * @param mutation
- */
-export function makeOnSubmit<TData, FormValues>(
-  mutation: (
-    options?: MutationFunctionOptions<TData, FormValues>
-  ) => Promise<FetchResult<TData>>
-): (variables: FormValues) => Promise<FinalFormSubmissionResult<FormValues>> {
-  const baseFn = makeMutationRequest(mutation);
-  return async (variables: FormValues) => {
-    const { errors } = await baseFn(variables);
-    return errors;
-  };
 }
